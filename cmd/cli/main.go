572d0242480960cc0ec63f7ddd71639dcf66ebed
Alex Dițu <ditu.alexandru@gmail.com>
2023-08-20 15:35:33 +0300

    cmd/cli/main.go: add stop signal handler and traverse 1st folder

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"io/fs"
 	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -30,7 +35,41 @@ func initLogger(verbose bool) {
 func run(args programArguments) error {
 	initLogger(args.enableLogs)
 
-	log.Infof("started")
+	// catch interrupt signals
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	generateDiff(ctx, args.dirPathA, args.dirPathB)
+
+	// restore default behavior to interrupt signals
+	stop()
+
+	return nil
+}
+
+func generateDiff(ctx context.Context, folderA, folderB string) error {
+	log.Infof("Traversing folderA: %s\n", folderA)
+
+	err := filepath.Walk(folderA, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			log.Errorf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if ctx.Err() != nil {
+			log.Debugf("Signaled to stop, reached path: %s", path)
+			return fs.SkipAll
+		}
+
+		log.Infof("visited file or dir: %q\n", path)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Errorf("error walking the path %q: %v\n", folderA, err)
+		return err
+	}
 
 	return nil
 }
